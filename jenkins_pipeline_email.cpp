5d04df7ccf8cbc31aba28bb2daab39cfd7049ad7
s223200581 <142364255+s223200581@users.noreply.github.com>
2023-09-06 14:49:34 +1000

    Update jenkins_pipeline_email.cpp

--- jenkins_pipeline_email.cpp
+++ jenkins_pipeline_email.cpp
@@ -10,40 +10,58 @@ pipeline{
             steps{
                 echo "fetch the source code from the ${DIRECTORYPATH}"
                 echo "compile the code and generate any necessary artifacts"
-            }
-                post{
-                    success{
-                        mail to: "ceemalakaushik22@gmail.com"
-                        subject: "build status email"
-                        body: "john was successful!!!"
-                    }
+                echo "The code is build usint the built automation tool named John"
                 }
-        }
-        stage('Test'){
+            }
+        
+        stage(' Unit and integration Tests'){
             steps{
             echo "unit tests"
             echo "integration test"
+            echo "john used the automation tool"
+            }
+            post{
+                success{
+                    mail to: "ceemalakaushik22@gmail.com"
+                    subject: "test status email"
+                    body: "test is completed"
+                }
             }
         }
-        stage('code'){
+        stage('code analysis'){
             steps{
                 echo "check the quality of the code"
+                echo " SonarQube was used as a tool "
             }
         }
-        stage('Deploy'){
+        stage('Security scan'){
             steps{
-                echo "deploy the application to a ${TESTINGENVIRONMENT}"
+                echo "VeraCode used for security scan"
+            }
+            post{
+                success{
+                    mail to: "ceemalakaushik22@gmail.com"
+                    subject: "security scan stage"
+                    body: "security scan completed"
+                }
             }
         }
-        stage('Approval'){
+        stage('Deploy to staging'){
            steps{
-            sleep time: 10, unit: "SECONDS"
+            echo "In Jenkins-deploy the application to a staging server"
            }
         }
-            stage ('Deploy to Production'){
+        stage ('Integration Tests on staging'){
+            steps{
+                echo " run the integration tests on the staging environment to ensure the application functions as expected in a production like environment"
+            }
+        }
+        stage ('Deploy to Production'){
                 steps{
                     echo "DEPLOY the code to ${PRODUCTIONENVIRONMENT}"
+                    echo "Deploy the application to a production server"
                 }
             }
         }
     }
+
